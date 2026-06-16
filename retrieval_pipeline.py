f2a898f720f5ca6866a6981158b7e95da481f1dd
nasimul007 <nasimul.hasan.aiub@gmail.com>
2026-06-16 14:33:23 +0600

    retrieval pipeline added

---
+++ retrieval_pipeline.py
@@ -0,0 +1,36 @@
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+
+persist_directory = "db/chroma_db"
+embedding_model = OllamaEmbeddings(model="embeddinggemma:latest")
+
+db = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw:space": "cosine"}
+)
+
+query = "What was the original name of Microsoft before it became Microsoft?"
+
+retriever = db.as_retriever(search_kwargs={"k": 3})
+relevant_docs = retriever.invoke(query)
+
+print(f"User Query: {query}")
+# Display results
+print("--- Context ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+
+
+# Synthetic Questions: 
+
+# 1. "What was NVIDIA's first graphics accelerator called?"
+# 2. "Which company did NVIDIA acquire to enter the mobile processor market?"
+# 3. "What was Microsoft's first hardware product release?"
+# 4. "How much did Microsoft pay to acquire GitHub?"
+# 5. "In what year did Tesla begin production of the Roadster?"
+# 6. "Who succeeded Ze'ev Drori as CEO in October 2008?"
+# 7. "What was the name of the autonomous spaceport drone ship that achieved the first successful sea landing?"
+# 8. "What was the original name of Microsoft before it became Microsoft?"
+# 9. "When did Microsoft reached a trillion-dollar market cap?"
